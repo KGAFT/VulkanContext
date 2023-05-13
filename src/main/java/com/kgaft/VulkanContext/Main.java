63e1c717eb455f3cbf99eaf135ad1a4bf74a4593
KGAFT <kgaftkgaft@gmail.com>
2023-05-13 23:14:01 +0300

    Project setted up

---
+++ src/main/java/com/kgaft/VulkanContext/Main.java
@@ -0,0 +1,11 @@
+package com.kgaft.VulkanContext;
+
+
+import org.lwjgl.vulkan.VkInstanceCreateInfo;
+
+public class Main {
+    public static void main(String[] args) {
+        VkInstanceCreateInfo instanceCreateInfo = VkInstanceCreateInfo.calloc();
+
+    }
+}

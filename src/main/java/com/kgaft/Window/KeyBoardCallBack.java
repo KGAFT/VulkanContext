e1b60a823f55dc6fa62a51cb7c0c61cc7461da3c
KGAFT <you@example.com>
2023-05-15 22:22:38 +0300

    Added device suitabillity support

---
+++ src/main/java/com/kgaft/Window/KeyBoardCallBack.java
@@ -0,0 +1,6 @@
+package com.kgaft.Window;
+
+public interface KeyBoardCallBack {
+    int[] getKeyCodes();
+    void keyPressed(int keyCodeId);
+}
---
+++ src/main/java/com/kgaft/Window/MouseMovementCallBack.java
@@ -0,0 +1,6 @@
+package com.kgaft.Window;
+
+public interface MouseMovementCallBack {
+    int getWorkMode();
+    void mouseMoved(double x, double y);
+}

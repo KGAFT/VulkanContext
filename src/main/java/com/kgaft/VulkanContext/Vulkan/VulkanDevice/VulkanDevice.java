0a079e60fa542a1ad9449b1acc6414c6a37c1591
Daniil <kgaftkgaft@gmail.com>
2023-05-17 13:22:19 +0300

    Added command pool

--- src/main/java/com/kgaft/VulkanContext/Vulkan/VulkanDevice/VulkanDevice.java
+++ src/main/java/com/kgaft/VulkanContext/Vulkan/VulkanDevice/VulkanDevice.java
@@ -8,29 +8,23 @@ import java.util.HashSet;
 
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.system.MemoryStack;
-import org.lwjgl.vulkan.VkDevice;
-import org.lwjgl.vulkan.VkDeviceCreateInfo;
-import org.lwjgl.vulkan.VkDeviceQueueCreateInfo;
-import org.lwjgl.vulkan.VkInstance;
-import org.lwjgl.vulkan.VkPhysicalDevice;
-import org.lwjgl.vulkan.VkPhysicalDeviceFeatures;
-import org.lwjgl.vulkan.VkPhysicalDeviceProperties;
-import org.lwjgl.vulkan.VkQueue;
+import org.lwjgl.vulkan.*;
+
 
 import static org.lwjgl.vulkan.VK13.*;
 
-
 public class VulkanDevice {
-    public static LinkedHashMap<VkPhysicalDevice, VkPhysicalDeviceProperties> enumerateSupportedDevices(VkInstance instance, long surface){
+    public static LinkedHashMap<VkPhysicalDevice, VkPhysicalDeviceProperties> enumerateSupportedDevices(
+            VkInstance instance, long surface) {
         int[] deviceCount = new int[1];
         vkEnumeratePhysicalDevices(instance, deviceCount, null);
         MemoryStack stack = MemoryStack.stackPush();
         PointerBuffer pBuffer = stack.callocPointer(deviceCount[0]);
         vkEnumeratePhysicalDevices(instance, deviceCount, pBuffer);
         LinkedHashMap<VkPhysicalDevice, VkPhysicalDeviceProperties> result = new LinkedHashMap<>();
-        while(pBuffer.hasRemaining()){
+        while (pBuffer.hasRemaining()) {
             VkPhysicalDevice device = new VkPhysicalDevice(pBuffer.get(), instance);
-            if(DeviceSuitability.isDeviceSuitable(device, surface)){
+            if (DeviceSuitability.isDeviceSuitable(device, surface)) {
                 VkPhysicalDeviceProperties properties = VkPhysicalDeviceProperties.calloc();
                 vkGetPhysicalDeviceProperties(device, properties);
                 result.put(device, properties);
@@ -41,41 +35,45 @@ public class VulkanDevice {
     }
 
     private VkPhysicalDevice deviceToCreate;
-    private long renderSurface; 
+    private long renderSurface;
     private VkInstance vkInstance;
     private int sampleCount;
     private VkDevice device;
     private VkQueue graphicsQueue;
     private VkQueue presentQueue;
-    
-    public VulkanDevice(VkPhysicalDevice deviceToCreate, long surface, VkInstance instance, boolean debugDevice){
+    private long commandPool;
+
+    public VulkanDevice(VkPhysicalDevice deviceToCreate, long surface, VkInstance instance, boolean debugDevice) {
         this.deviceToCreate = deviceToCreate;
         this.renderSurface = surface;
         this.vkInstance = instance;
-        if(!DeviceSuitability.isDeviceSuitable(deviceToCreate, surface)){
+        if (!DeviceSuitability.isDeviceSuitable(deviceToCreate, surface)) {
             throw new UnsupportedOperationException("Error: the device you specified is not supported");
         }
         VkPhysicalDeviceProperties properties = VkPhysicalDeviceProperties.calloc();
         vkGetPhysicalDeviceProperties(deviceToCreate, properties);
-        this.sampleCount = properties.limits().framebufferColorSampleCounts() & properties.limits().framebufferDepthSampleCounts();
+        this.sampleCount = properties.limits().framebufferColorSampleCounts()
+                & properties.limits().framebufferDepthSampleCounts();
 
         properties.free();
         createLogicalDevice(debugDevice);
+        createCommandPool();
     }
 
-    private void createLogicalDevice(boolean enableLog){
-        try(MemoryStack stack = MemoryStack.stackPush()){
+    private void createLogicalDevice(boolean enableLog) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
             QueueFamilyIndices indices = DeviceSuitability.findQueueFamilies(deviceToCreate, renderSurface);
             HashSet<Integer> uniqueIndices = new HashSet<>();
             uniqueIndices.add(indices.graphicsFamily);
             uniqueIndices.add(indices.presentFamily);
-            VkDeviceQueueCreateInfo.Buffer queueCreateInfos = VkDeviceQueueCreateInfo.calloc(uniqueIndices.size(), stack);
+            VkDeviceQueueCreateInfo.Buffer queueCreateInfos = VkDeviceQueueCreateInfo.calloc(uniqueIndices.size(),
+                    stack);
             AtomicInteger counter = new AtomicInteger(0);
             FloatBuffer queuePriority = stack.callocFloat(1);
             queuePriority.put(1.0f);
             queuePriority.rewind();
 
-            uniqueIndices.forEach(index->{
+            uniqueIndices.forEach(index -> {
                 queueCreateInfos.get(counter.get()).sType$Default();
                 queueCreateInfos.get(counter.get()).queueFamilyIndex(index);
                 queueCreateInfos.get(counter.get()).pQueuePriorities(queuePriority);
@@ -88,7 +86,7 @@ public class VulkanDevice {
             List<String> exts = DeviceSuitability.getRequiredDeviceExtensions();
 
             PointerBuffer pExtensions = stack.callocPointer(exts.size());
-            exts.forEach(extension->{
+            exts.forEach(extension -> {
                 pExtensions.put(stack.UTF8Safe(extension));
             });
             pExtensions.rewind();
@@ -100,26 +98,74 @@ public class VulkanDevice {
 
             deviceCreateInfo.ppEnabledExtensionNames(pExtensions);
 
-            if(enableLog){
+            if (enableLog) {
                 deviceCreateInfo.ppEnabledLayerNames(stack.callocPointer(1));
                 deviceCreateInfo.ppEnabledLayerNames().put(stack.UTF8Safe("VK_LAYER_KHRONOS_validation"));
                 deviceCreateInfo.ppEnabledLayerNames().rewind();
             }
             PointerBuffer deviceResult = stack.callocPointer(1);
-            if(vkCreateDevice(deviceToCreate, deviceCreateInfo, null, deviceResult)!=VK_SUCCESS){
+            if (vkCreateDevice(deviceToCreate, deviceCreateInfo, null, deviceResult) != VK_SUCCESS) {
                 throw new RuntimeException("Failed to create device");
             }
             device = new VkDevice(deviceResult.get(), deviceToCreate, deviceCreateInfo);
             deviceResult.clear();
             deviceResult.rewind();
-            
+
             vkGetDeviceQueue(device, indices.graphicsFamily, 0, deviceResult);
             graphicsQueue = new VkQueue(deviceResult.get(), device);
             deviceResult.clear();
             deviceResult.rewind();
-            
+
             vkGetDeviceQueue(device, indices.presentFamily, 0, deviceResult);
             presentQueue = new VkQueue(deviceResult.get(), device);
         }
+    }
+
+    private void createCommandPool() {
+        QueueFamilyIndices indices = DeviceSuitability.findQueueFamilies(deviceToCreate, renderSurface);
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            VkCommandPoolCreateInfo createInfo = VkCommandPoolCreateInfo.calloc(stack);
+            createInfo.sType$Default();
+            createInfo.queueFamilyIndex(indices.graphicsFamily);
+            createInfo.flags(VK_COMMAND_POOL_CREATE_TRANSIENT_BIT | VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+            long[] result = new long[1];
+            vkCreateCommandPool(device, createInfo, null, result);
+            this.commandPool = result[0];
+        }
+    }
+
+    VkCommandBuffer beginSingleTimeCommands() {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            VkCommandBufferAllocateInfo allocInfo = VkCommandBufferAllocateInfo.calloc(stack);
+            allocInfo.sType$Default();
+            allocInfo.level(VK_COMMAND_BUFFER_LEVEL_PRIMARY);
+            allocInfo.commandPool(commandPool);
+            allocInfo.commandBufferCount(1);
+            
+            PointerBuffer tempRes = stack.callocPointer(1);
+            vkAllocateCommandBuffers(device, allocInfo, tempRes);
+            VkCommandBuffer result = new VkCommandBuffer(tempRes.get(), device);
+
+            VkCommandBufferBeginInfo beginInfo = VkCommandBufferBeginInfo.calloc(stack);
+            beginInfo.sType$Default();
+            beginInfo.flags(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
+            vkBeginCommandBuffer(result, beginInfo);
+            return result;
+        }
+    }
+
+    void endSingleTimeCommands(VkCommandBuffer cmd){
+        vkEndCommandBuffer(cmd);
+        try(MemoryStack stack = MemoryStack.stackPush()){
+            VkSubmitInfo.Buffer submitInfo = VkSubmitInfo.calloc(1, stack);
+            Vk
+            submitInfo.sType$Default();
+            submitInfo.pCommandBuffers(cmd);
+            vkQueueSubmit(graphicsQueue, submitInfo, VK_NULL_HANDLE);
+            vkQueueWaitIdle(graphicsQueue);
+
+            vkFreeCommandBuffer(device, commandPool, cmd);
+        }
+        
     }
 }

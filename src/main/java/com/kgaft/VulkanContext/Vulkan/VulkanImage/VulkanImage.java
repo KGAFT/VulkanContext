67c581e7b4879f3827a917dfa5f2b4771b0e3336
kgaft <kgaftkgaft@gmail.com>
2023-08-02 16:02:05 +0300

    Added changing layout into vulkan image

--- src/main/java/com/kgaft/VulkanContext/Vulkan/VulkanImage/VulkanImage.java
+++ src/main/java/com/kgaft/VulkanContext/Vulkan/VulkanImage/VulkanImage.java
@@ -4,13 +4,21 @@ import org.lwjgl.vulkan.*;
 
 import com.kgaft.VulkanContext.Exceptions.BuilderNotPopulatedException;
 import com.kgaft.VulkanContext.Vulkan.VulkanDevice.VulkanDevice;
+import com.kgaft.VulkanContext.Vulkan.VulkanDevice.VulkanQueue;
 
 import org.lwjgl.system.MemoryStack;
 
+import static org.lwjgl.vulkan.VK10.VK_ACCESS_SHADER_WRITE_BIT;
+import static org.lwjgl.vulkan.VK10.VK_ACCESS_TRANSFER_WRITE_BIT;
+import static org.lwjgl.vulkan.VK10.VK_IMAGE_ASPECT_COLOR_BIT;
 import static org.lwjgl.vulkan.VK10.VK_IMAGE_TYPE_2D;
+import static org.lwjgl.vulkan.VK10.VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+import static org.lwjgl.vulkan.VK10.VK_QUEUE_FAMILY_IGNORED;
+import static org.lwjgl.vulkan.VK10.VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
 import static org.lwjgl.vulkan.VK10.VK_SUCCESS;
 import static org.lwjgl.vulkan.VK10.vkAllocateMemory;
 import static org.lwjgl.vulkan.VK10.vkBindImageMemory;
+import static org.lwjgl.vulkan.VK10.vkCmdPipelineBarrier;
 import static org.lwjgl.vulkan.VK10.vkCreateImage;
 import static org.lwjgl.vulkan.VK10.vkGetImageMemoryRequirements;
 import static org.lwjgl.vulkan.VK13.*;
@@ -23,6 +31,9 @@ public class VulkanImage {
     private int imageTiling;
     private int sharingMode;
     private int samples;
+    private int accessMask = 0;
+    private int shaderStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+
     public VulkanImage(VulkanDevice device, VulkanImageBuilder builder) throws BuilderNotPopulatedException {
         this.device = device;
         builder.checkBuilder();
@@ -37,6 +48,99 @@ public class VulkanImage {
             this.imageMemory = createImageMemory(stack, device, builder.getImageMemoryProperties(), image);
         }
         
+    }
+
+    public VulkanDevice getDevice() {
+        return device;
+    }
+
+
+
+    public long getImage() {
+        return image;
+    }
+
+
+
+    public long getImageMemory() {
+        return imageMemory;
+    }
+
+
+
+    public int getImageLayout() {
+        return imageLayout;
+    }
+
+
+
+    public int getImageTiling() {
+        return imageTiling;
+    }
+
+
+
+    public int getSharingMode() {
+        return sharingMode;
+    }
+
+
+
+    public int getSamples() {
+        return samples;
+    }
+
+    public void changeLayout(int targetLayout, int targetAccessMask, int targetShaderStage, VulkanQueue queue){
+        try(MemoryStack stack = MemoryStack.stackPush()){
+            changeLayout(stack, targetLayout, targetAccessMask, targetShaderStage, queue);
+        }
+    }
+
+    public void changeLayout(MemoryStack stack, int targetLayout, int targetAccessMask, int targetShaderStage, VulkanQueue queue){
+        VkCommandBuffer cmd = queue.beginSingleTimeCommands(stack);
+        changeLayout(stack, targetLayout, targetAccessMask, targetShaderStage, cmd);
+        queue.endSingleTimeCommands(cmd, stack);
+    }
+
+    public void changeLayout(MemoryStack stack, int targetLayout, int targetAccessMask, int targetShaderStage, VkCommandBuffer cmd){
+        VkImageMemoryBarrier.Buffer barrier = VkImageMemoryBarrier.calloc(1, stack);
+        barrier.sType(VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER);
+        barrier.oldLayout(this.imageLayout);
+        barrier.newLayout(targetLayout);
+        barrier.srcQueueFamilyIndex(VK_QUEUE_FAMILY_IGNORED);
+        barrier.dstQueueFamilyIndex(VK_QUEUE_FAMILY_IGNORED);
+        barrier.image(image);
+        barrier.subresourceRange().aspectMask(VK_IMAGE_ASPECT_COLOR_BIT);
+        barrier.subresourceRange().baseMipLevel(0);
+        barrier.subresourceRange().levelCount(1);
+        barrier.subresourceRange().baseArrayLayer(0);
+        barrier.subresourceRange().layerCount(1);
+
+        barrier.srcAccessMask(this.accessMask);
+        barrier.dstAccessMask(targetAccessMask);
+
+
+        vkCmdPipelineBarrier(cmd,
+                this.shaderStage, targetShaderStage,
+                0,
+                null,
+                null,
+                barrier);
+        this.accessMask = targetAccessMask;
+        this.imageLayout = targetLayout;
+        this.shaderStage = targetShaderStage;        
+
+    }
+
+
+    public int getAccessMask() {
+        return accessMask;
+    }
+
+
+
+    public int getShaderStage() {
+        return shaderStage;
     }
 
     private long createImage(MemoryStack stack, VulkanImageBuilder builder) {
